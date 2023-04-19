2639337e7d9195d5a651c8f9025d9e65eafc5b96
studioOwol <from.owol@gmail.com>
2023-04-19 13:33:33 +0900

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Nahyun on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

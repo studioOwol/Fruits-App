c106c31e79ee4f38fa81dbeb6a204ac11e71a88b
studioOwol <from.owol@gmail.com>
2023-04-19 15:26:49 +0900

    Create List View

--- Fruits/App/ContentView.swift
+++ Fruits/App/ContentView.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: - PROPERTY
+    
+    var fruits: [Fruit] = fruitsData
+    
+    //MARK: - BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        NavigationView{
+            List{
+                ForEach(fruits.shuffled()) { fruit in
+                    FruitRowView(fruit: fruit)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
+        }//: Navigation
     }
 }
 
+//MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }

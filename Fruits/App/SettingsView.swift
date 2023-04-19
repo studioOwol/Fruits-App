32c3fd74107fb1b513d20ece118dd6e8f26f0225
studioOwol <from.owol@gmail.com>
2023-04-19 17:32:09 +0900

    Settings View - Part 3

--- Fruits/App/SettingsView.swift
+++ Fruits/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
     //MARK: - PROPERTY
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     //MARK: - BODY
     var body: some View {
@@ -21,7 +22,7 @@ struct SettingsView: View {
                     
                     GroupBox(
                         label:
-                           SettingsLabelView(labelText: "Fruit", labelImage: "info.circle")
+                            SettingsLabelView(labelText: "Fruit", labelImage: "info.circle")
                     ){
                         Divider().padding(.vertical, 4)
                         HStack(alignment: .center, spacing: 10) {
@@ -36,6 +37,35 @@ struct SettingsView: View {
                         }
                     }
                     //MARK: - Section 2
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")){
+                            Divider().padding(.vertical, 4)
+                            
+                            Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                                .padding(.vertical, 8)
+                                .frame(minHeight: 60)
+                                .layoutPriority(1)
+                                .font(.footnote)
+                                .multilineTextAlignment(.leading)
+                            
+                            Toggle(isOn: $isOnboarding){
+                                if isOnboarding {
+                                    Text("Restarted".uppercased())
+                                        .fontWeight(.bold)
+                                        .foregroundColor(Color.green)
+                                } else {
+                                    Text("Restart".uppercased())
+                                        .fontWeight(.bold)
+                                        .foregroundColor(Color.secondary)
+                                }
+                            }
+                            .padding()
+                            .background(
+                                Color(UIColor.tertiarySystemBackground)
+                                    .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                            )
+                        }
                     
                     //MARK: - Section 3
                     

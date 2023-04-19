0c3e5ef1b41bd8609f7889a7237f05f16feac4b1
studioOwol <from.owol@gmail.com>
2023-04-19 15:13:50 +0900

    How to use App Storage and understand the new SwiftUI App life cycle

--- Fruits/View/StartButtonView.swift
+++ Fruits/View/StartButtonView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct StartButtonView: View {
     //MARK: - PROPERTY
     
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
+    
     //MARK: - BODY
     var body: some View {
         Button(action: {
-            print("Exit the onboarding")
+            isOnboarding = false
+            
         }) {
             HStack(spacing: 8) {
                 Text("Start")
